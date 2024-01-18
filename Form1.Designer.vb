4a3de0479f2b36b946f39aab7835f83cd13d4cc7
tshir <tshir@MSI>
2024-01-18 14:23:01 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        ListBox1 = New ListBox()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        ComboBox1 = New ComboBox()
+        Label1 = New Label()
+        Button2 = New Button()
+        Button3 = New Button()
+        SuspendLayout()
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 15
+        ListBox1.Location = New Point(111, 59)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(120, 94)
+        ListBox1.TabIndex = 0
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(341, 59)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 1
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(78, 186)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 2
+        Button1.Text = "Add"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(327, 121)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(121, 23)
+        ComboBox1.TabIndex = 3
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(345, 195)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(41, 15)
+        Label1.TabIndex = 4
+        Label1.Text = "Label1"
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(179, 185)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 5
+        Button2.Text = "Remove "
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(128, 232)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(75, 23)
+        Button3.TabIndex = 6
+        Button3.Text = "Clear all"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Label1)
+        Controls.Add(ComboBox1)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        Controls.Add(ListBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,66 @@
+﻿Public Class Form1
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        ' Change the color of the selected word in the ListBox
+        If ListBox1.SelectedIndex <> -1 Then
+            Dim selectedColor As Color = GetSelectedColor()
+            ListBox1.ForeColor = selectedColor
+            Label1.ForeColor = selectedColor
+            Label1.Text = "Selected Word: " & ListBox1.SelectedItem.ToString()
+        End If
+
+
+    End Sub
+    Private Function GetSelectedColor() As Color
+        ' Get the selected color from the ComboBox
+        Select Case ComboBox1.SelectedItem.ToString()
+            Case "Red"
+                Return Color.Red
+            Case "Green"
+                Return Color.Green
+            Case "Blue"
+                Return Color.Blue
+            Case "Yellow"
+                Return Color.Yellow
+            Case Else
+                Return Color.Black ' Default to Black if no color is selected
+        End Select
+    End Function
+
+
+
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Populate color options in ComboBox
+        ComboBox1.Items.Add("Red")
+        ComboBox1.Items.Add("Green")
+        ComboBox1.Items.Add("Blue")
+        ComboBox1.Items.Add("Yellow")
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Add the entered word to the ListBox
+        If Not String.IsNullOrWhiteSpace(TextBox1.Text) Then
+            ListBox1.Items.Add(TextBox1.Text)
+            TextBox1.Clear()
+        Else
+            MessageBox.Show("Please enter a word.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If ListBox1.SelectedIndex <> -1 Then
+            ListBox1.Items.RemoveAt(ListBox1.SelectedIndex)
+            ListBox1.Text = "Selected Word: "
+        Else
+            MessageBox.Show("Please select a word to remove.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ListBox1.Items.Clear()
+        Label1.Text = "Selected Word: "
+    End Sub
+End Class
